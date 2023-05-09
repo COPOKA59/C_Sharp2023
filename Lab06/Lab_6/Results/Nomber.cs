79d2bb4f09b3c7a1b5cf9b93b6935b3b907faa9e
COPOKA59 <133018391+COPOKA59@users.noreply.github.com>
2023-05-09 23:24:34 +0500

    Add files via upload

---
+++ Lab06/Lab_6/Results/Nomber.cs
@@ -0,0 +1,29 @@
+﻿
+// Класс для создания комплексного числа
+
+namespace Nomber
+{
+    public class Nomber
+    {
+        // Комплексное число имеет вид: a + b*i, где a,b - числа, i - мнимая единица
+        public double a;
+        public double b;
+
+        public Nomber()
+        {
+            this.a = 0;
+            this.b = 0;
+        }
+
+        public Nomber(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("{0} + {1}i", a, b);
+        }
+    }
+}
