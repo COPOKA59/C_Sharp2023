8e19e3e07b13185180f6a7613cb0d5c1fba84c5c
COPOKA59 <133018391+COPOKA59@users.noreply.github.com>
2023-05-09 22:51:10 +0500

    Add files via upload

---
+++ Lab03/Lab_3/Lab_3/Program.cs
@@ -0,0 +1,89 @@
+﻿
+/* Макарова Полина ПМИ-2
+ * Вариант 10
+ * Лабораторная работа 3
+ */
+
+/* 1. Напишите алгоритм накопления суммы элементов одномерного массива, 
+ * а затем создайте по нему программу. Элементы вводите с клавиатуры. 
+ * Размер массива 10. Вывод результатов форматный.  
+ * 
+ * 2. Напишите алгоритм поиска максимального элемента двумерного 
+ * массива (10, 12). Элементы массива генерируется с помощью класса Random. 
+ * Для вывода элементов сгенерированного массива использовать цикл foreach. 
+ * Создайте по нему программу. Статья как задать случайные 
+ * числа http://plssite.ru/csharp/csharp_random_article.html 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+Console.SetWindowSize(63, 40); 
+Console.BackgroundColor = ConsoleColor.White; 
+Console.Clear(); 
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine("  |  Индивидуальное задание 1  |\n");
+
+// Объявление переменных
+double S = 0;
+// Объявление массива A
+double[] A = new double[10];
+// Ввод элементов массива X(10)
+Console.WriteLine("\nВведите элементы массива A");
+
+for (int i = 0; i < 10; i++)
+{
+    A[i] = double.Parse(Console.ReadLine());
+}
+// Накопление суммы элементов массива
+for (int i = 0; i < 10; i++)
+{
+    S += A[i];
+}
+// Вывод суммы эл.массива
+Console.WriteLine("\n Сумма элементов массива A равна: {0}", S);
+
+
+Console.WriteLine("\n\n  |  Индивидуальное задание 2  |\n\n");
+// Создание двумерного массива
+double[,] B = new double[10, 12];
+// Создание объекта для генерации чисел
+Random rnd = new Random();
+Console.WriteLine("\nЭлементы массива B");
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+    // Получить случайное число
+    B[i, j] = rnd.Next(-100, 100);
+    }
+}
+
+int N = 0, M = 0;
+double max = B[0, 0];
+// Поиск максимального элемента массива
+// Вывод в консоль массива
+foreach (int i in Enumerable.Range(0, B.GetLength(0)))
+{
+    foreach (int j in Enumerable.Range(0, B.GetLength(1)))
+    {
+        Console.Write("{0,5}", B[i, j]);
+        // Поиск максимального элемента массива
+        if (B[i, j] > max)
+        {
+            max = B[i, j];
+            N = i;
+            M = j;
+        }
+
+    }
+    Console.WriteLine();
+}
+
+// Вывод в консоль мак.эл.массива
+Console.WriteLine("\nМаксимальный элемент массива: {0}", max);
+Console.WriteLine("\nЭллемент массива [{0}, {1}]", N+1, M+1);
+
---
+++ Lab04/Lab_4/Lab_4/Program.cs
@@ -0,0 +1,126 @@
+﻿/* Макарова Полина ПМИ-2
+ * Вариант 10
+ * Лабораторная работа 4
+ */
+
+/* 1. Спроектируйте класс (ы), наполните его (их) требуемой функциональностью, 
+ * продемонстрируйте работоспособность программы 
+ * Класс "Цилиндр". 
+ * Реализовать ввод и вывод полей данных, вычисление объёма, площади поверхности,
+ * а так же вывод информации об объекте.
+ * 
+ * 2. Модифицируйте вашу программу таким образом, чтобы она использовала информацию, 
+ * полученную в том числе и на лекциях. А именно (по возможности и целесообразности) 
+ * продемонстрировать: инкапсуляцию; применение полей, свойств, конструкторов; 
+ * включение, вложение, наследование классов; перегрузки методов/конструкторов; 
+ * возможность задание параметров «по умолчанию» и с клавиатуры; 
+ * задание параметров консоли и форматный вывод. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_4
+{
+    class Cylinder
+    {
+        // Поля данных: r - радиус, h - высота, pi - константа пи
+        private double r, h;
+        private const double pi = 3.141;
+
+        // Конструктор по умолчанию
+        public Cylinder()
+        {
+            r = 1;
+            h = 1;
+        }
+
+        // Конструктор
+        public Cylinder(double ra, double ha)
+        {
+            r = ra;
+            h = ha;
+        }
+
+        // Метод для установки и считывания значений полей
+        public double H
+        {
+            get { return this.h; }
+            set { this.h = value; }
+        }
+
+        public double R
+        {
+            get { return this.r; }
+            set { this.r = value; }
+        }
+
+        // Метод для вычисления объёма цилиндра
+        private double GetV()
+        {
+            return pi * r * r * h;
+        }
+
+        // Метод для вычисления площади поверхности цилиндра
+        private double GetS()
+        {
+            return 2 * pi * r * h + 2 * pi * r * r;
+        }
+
+        // Метод для вывода полной информации об объекте в консоль
+        public void PrintFullInformation()
+        {
+            string str = "\n       --------------------------------\n" +
+                           "       |                              |\n" +
+                           "       |        Объект Цилиндр        |\n" +
+                           "       |                              |\n" +
+                           "       --------------------------------\n";
+            Console.WriteLine(str);
+            Console.WriteLine("   Данные Цилиндра:\n\n      Высота = {0:F3}\n      Радиус = {1:F3}\n", H, R);
+            Console.WriteLine("\n   Объём Цилиндра = {0:F3}\n", GetV());
+            Console.WriteLine("   Площадь поверхности Цилиндра = {0:F3}\n", GetS());
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Параметры консоли
+            Console.Title = "Цилиндр";
+            Console.SetWindowSize(60, 30);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            string a;
+            double r, h;
+
+            // Создание объекта Цилиндр
+            Cylinder C;
+            C = new Cylinder();
+            Console.Write("            Введите 1 для ввода данных из консили\n" +
+                          "    или любую цифру для задания параметров по умолчанию\n   ");
+            a = Console.ReadLine();
+
+            // Ввод данных из консоли
+            if (Convert.ToDouble(a) == 1)
+            {
+                Console.Write("\n   Введите высоту Цилиндра = ");
+                a = Console.ReadLine();
+                C.H = (Convert.ToDouble(a));
+                Console.Write("\n   Введите радиус Цилиндра = ");
+                a = Console.ReadLine();
+                C.R = (Convert.ToDouble(a));
+            }
+
+            // Вывод информации об объекте
+            C.PrintFullInformation();
+
+            Console.ReadKey();
+
+        }
+    }
+}
