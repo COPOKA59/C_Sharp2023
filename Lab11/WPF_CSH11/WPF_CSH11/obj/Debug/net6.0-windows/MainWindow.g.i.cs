7a20581bddb7464e348acfeb99394a0d84998689
COPOKA59 <133018391+COPOKA59@users.noreply.github.com>
2023-06-19 23:34:33 +0500

    One

--- Lab11/WPF_CSH11/WPF_CSH11/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Lab11/WPF_CSH11/WPF_CSH11/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -189,7 +189,7 @@ namespace WPF_CSH11 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF_CSH11;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF_CSH11;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
